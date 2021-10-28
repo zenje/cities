eff7be5f5dfba6206f1b386ef1e318e3336aa209
Jessica Zeng <jessicaczeng@gmail.com>
2021-10-28 00:44:38 -0700

    format displayed population

--- src/components/CityMarker.js
+++ src/components/CityMarker.js
@@ -32,6 +32,26 @@ const getColor = (population) => {
   return COLORS[index];
 };
 
+const nFormatter = (num, digits) => {
+  const lookup = [
+    { value: 1, symbol: "" },
+    { value: 1e3, symbol: "k" },
+    { value: 1e6, symbol: "M" },
+    { value: 1e9, symbol: "G" },
+    { value: 1e12, symbol: "T" },
+    { value: 1e15, symbol: "P" },
+    { value: 1e18, symbol: "E" },
+  ];
+  const rx = /\.0+$|(\.[0-9]*[1-9])0+$/;
+  const item = lookup
+    .slice()
+    .reverse()
+    .find((item) => num >= item.value);
+  return item
+    ? (num / item.value).toFixed(digits).replace(rx, "$1") + item.symbol
+    : "0";
+};
+
 const CityMarker = ({ coordinates, name, population, setTooltipContent }) => {
   const initialRadius = getRadius(population);
   const hoveredRadius = initialRadius * 2;
@@ -45,9 +65,8 @@ const CityMarker = ({ coordinates, name, population, setTooltipContent }) => {
         fillOpacity={MARKER_OPACITY}
         style={{ transition: "0.2s ease-in-out" }}
         onMouseEnter={() => {
-          setTooltipContent(name + " " + population);
+          setTooltipContent(name + " " + nFormatter(population, 1));
           setRadius(hoveredRadius);
-          console.log(name + " " + population);
         }}
         onMouseLeave={() => {
           setTooltipContent("");
