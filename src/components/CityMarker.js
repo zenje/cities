90a30f6f16f072beba67fe563bed90428d60ecef
Jessica Zeng <jessicaczeng@gmail.com>
2021-10-27 17:57:45 -0700

    add tooltip and fade transition for CityMarkers

--- src/components/CityMarker.js
+++ src/components/CityMarker.js
@@ -1,55 +1,62 @@
 import React, { useState } from "react";
 import { Marker } from "react-simple-maps";
 
-const CityMarker = ({ coordinates, name, population, scale }) => {
-  const getRadius = (population) => Math.sqrt(population / Math.PI) / 200;
-  const getColor = (population) => {
-    const colors = [
-      "#10497E",
-      "#1D7387",
-      "#2A9D8F",
-      "#EE8959",
-      "#E76F51",
-      "#E64C70",
-      "#E52457",
-    ];
-    let index = 0;
-    if (population >= 10000000) {
-      index = 0;
-    } else if (population >= 5000000) {
-      index = 1;
-    } else if (population >= 2000000) {
-      index = 2;
-    } else if (population >= 1000000) {
-      index = 3;
-    } else if (population >= 500000) {
-      index = 4;
-    } else if (population >= 100000) {
-      index = 5;
-    } else {
-      index = 6;
-    }
-    return colors[index];
-  };
+const MARKER_OPACITY = "0.7";
+const getRadius = (population) => Math.sqrt(population / Math.PI) / 200;
+const getColor = (population) => {
+  const COLORS = [
+    "#10497E",
+    "#1D7387",
+    "#2A9D8F",
+    "#EE8959",
+    "#E76F51",
+    "#E64C70",
+    "#E52457",
+  ];
+  let index = 0;
+  if (population >= 10000000) {
+    index = 0;
+  } else if (population >= 5000000) {
+    index = 1;
+  } else if (population >= 2000000) {
+    index = 2;
+  } else if (population >= 1000000) {
+    index = 3;
+  } else if (population >= 500000) {
+    index = 4;
+  } else if (population >= 100000) {
+    index = 5;
+  } else {
+    index = 6;
+  }
+  return COLORS[index];
+};
 
+const CityMarker = ({
+  coordinates,
+  name,
+  population,
+  scale,
+  setTooltipContent,
+}) => {
   const initialRadius = getRadius(population);
   const hoveredRadius = initialRadius * 2;
   let [radius, setRadius] = useState(initialRadius);
-  const isVisible = population > 1000000 / scale;
-  const opacity = isVisible ? "0.7" : "0";
   const color = getColor(population);
   return (
     <Marker coordinates={coordinates}>
       <circle
         r={`${radius}px`}
         fill={color}
-        fillOpacity={opacity}
+        fillOpacity={MARKER_OPACITY}
         style={{ transition: "0.2s ease-in-out" }}
         onMouseEnter={() => {
+          setTooltipContent(name + " " + population);
           setRadius(hoveredRadius);
           console.log(name + " " + population);
         }}
         onMouseLeave={() => {
+          setTooltipContent("");
           setRadius(initialRadius);
         }}
       />
--- src/components/WorldMap.js
+++ src/components/WorldMap.js
@@ -1,10 +1,13 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, memo } from "react";
 import {
   ComposableMap,
   ZoomableGroup,
   Geographies,
   Geography,
 } from "react-simple-maps";
+import ReactTooltip from "react-tooltip";
+import { CSSTransition, TransitionGroup } from "react-transition-group";
+
 import CityMarker from "./CityMarker";
 import GEO_JSON from "../data/world-110m.json";
 import CITY_DATA from "../data/cities15000.txt";
@@ -28,29 +31,43 @@ const getCities = async (setCities) => {
 
 const getMarkers = (setMarkers, cities) => {
   if (cities) {
-    setMarkers(Object.values(cities));
+    // filter out cities with population < 50000 to optimize rendering
+    let markers = Object.values(cities).filter((marker) => marker[14] >= 50000);
+    setMarkers(markers);
   }
 };
 
-const renderMarkers = (markers, scale) => {
+const renderMarkers = (markers, scale, setTooltipContent) => {
   if (markers) {
-    return markers
-      .filter((marker) => marker[14] >= 50000)
-      .map((marker, index) => (
-        <CityMarker
-          key={index}
-          name={marker[2]}
-          population={marker[14]}
-          coordinates={[marker[5], marker[4]]}
-          scale={scale}
-        />
-      ));
+    return (
+      <TransitionGroup component={null}>
+        {markers
+          .filter((marker) => marker[14] > 1000000 / scale)
+          .map((marker) => (
+            <CSSTransition
+              key={marker[0]}
+              classNames="city-marker"
+              timeout={500}
+            >
+              <CityMarker
+                key={marker[0]}
+                name={marker[2]}
+                population={marker[14]}
+                coordinates={[marker[5], marker[4]]}
+                scale={scale}
+                setTooltipContent={setTooltipContent}
+              />
+            </CSSTransition>
+          ))}
+      </TransitionGroup>
+    );
   }
 };
 
 const WorldMap = () => {
   let [markers, setMarkers] = useState([]);
   let [scale, setScale] = useState(1);
+  let [tooltipContent, setTooltipContent] = useState("");
 
   useEffect(() => {
     // useEffect is synchronous, call async function within useEffect to fetch data
@@ -62,7 +79,7 @@ const WorldMap = () => {
 
   return (
     <div>
-      <ComposableMap>
+      <ComposableMap data-tip="">
         <ZoomableGroup
           onMoveEnd={(...args) => {
             setScale(args[0].zoom);
@@ -85,11 +102,12 @@ const WorldMap = () => {
               })
             }
           </Geographies>
-          {renderMarkers(markers, scale)}
+          {renderMarkers(markers, scale, setTooltipContent)}
         </ZoomableGroup>
       </ComposableMap>
+      <ReactTooltip>{tooltipContent}</ReactTooltip>
     </div>
   );
 };
 
-export default WorldMap;
+export default memo(WorldMap);
